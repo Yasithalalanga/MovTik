85ca71d006cd5cc671f8b8a96b6a24e4d604d6f3
Yasitha Gamage <yasithamaxs@gmail.com>
2021-03-21 11:33:43 +0530

    Register Activity Layout

--- app/src/main/java/com/livecodex/movtik/MainActivity.java
+++ app/src/main/java/com/livecodex/movtik/MainActivity.java
@@ -2,7 +2,9 @@ package com.livecodex.movtik;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void registerMovieActivity(View view) {
+        Intent registerIntent = new Intent(MainActivity.this, RegisterMovieActivity.class);
+        startActivity(registerIntent);
+    }
+
+    public void displayMoviesActivity(View view) {
+        Intent displayIntent = new Intent(MainActivity.this, DisplayMovieActivity.class);
+        startActivity(displayIntent);
+    }
+
+    public void favouritesActivity(View view) {
+        Intent favouritesIntent = new Intent(MainActivity.this, FavouritesActivity.class);
+        startActivity(favouritesIntent);
+    }
+
+    public void editMoviesActivity(View view) {
+        Intent editMoviesIntent = new Intent(MainActivity.this, EditMovieActivity.class);
+        startActivity(editMoviesIntent);
+    }
+
+    public void searchActivity(View view) {
+        Intent searchIntent = new Intent(MainActivity.this, SearchActivity.class);
+        startActivity(searchIntent);
+    }
+
+    public void ratingsActivity(View view) {
+        Intent ratingsIntent = new Intent(MainActivity.this, RatingActivity.class);
+        startActivity(ratingsIntent);
     }
 }
